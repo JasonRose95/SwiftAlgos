97f227f4eb6f4fe55a12b20c0240292a47ddc1b5
Jason Rosenthal <jasonrosenthalr@gmail.com>
2022-03-08 14:37:16 -0800

    Finish algos

--- selectionSort.swift
+++ selectionSort.swift
@@ -1,6 +1,24 @@
 class Program {
   func selectionSort(array: inout [Int]) -> [Int] {
-    // Write your code here.
-    return []
+    var currentIndex = 0
+
+    while currentIndex < array.count - 1{
+      var indexOfSmallest = currentIndex
+
+      for i in currentIndex + 1 ..< array.count {
+        if array[indexOfSmallest] > array[i] {
+          indexOfSmallest = i
+        }
+      }
+      swapHelper(currentIndex, indexOfSmallest, &array)
+      currentIndex = currentIndex + 1
+    }
+    return array
+  }
+  func swapHelper(_ firstIndex: Int, _ secondIndex: Int, _ array: inout [Int]){
+    let temp = array[secondIndex]
+    
+    array[secondIndex] = array[firstIndex]
+    array[firstIndex] = temp
   }
 }
